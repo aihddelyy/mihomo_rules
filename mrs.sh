663bba46f4b4f0880f637748143e2521ba7933b2
aihddelyy <aihdce@gmail.com>
2025-04-06 03:32:38 +0800

    init

---
+++ mrs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cd rules/yaml
+find . -name "*.yaml" | while read -r file; do    
+    filename=$(basename "$file" .yaml)
+
+    if [[ "$filename" == *ip* ]]; then
+        param="ipcidr"
+    else
+        param="domain"
+    fi
+
+    output_file="$filename.mrs"
+
+    /usr/local/bin/mihomo convert-ruleset "$param" yaml "$file" "$output_file"
+
+    if [[ $? -eq 0 ]]; then
+        echo "文件 $file 转换成功为 $output_file"
+    else
+        echo "文件 $file 转换失败"
+    fi
+done
+
+echo "所有文件转换完成！"
+
+mv *.mrs ../mrs/
