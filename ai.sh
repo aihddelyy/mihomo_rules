fee2c99dfb25707cc57706850e41a1b5f380b17a
aihddelyy <aihdce@gmail.com>
2025-09-16 09:28:23 +0800

    up

--- ai.sh
+++ ai.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-# 优化后的脚本，适用于 OpenWrt 环境
+# 切换到nikki运行目录
 cd /etc/nikki/run
-# 下载并解压 mihomo
+
+# 下载并解压mihomo
 wget -O mihomo.gz "https://gh-proxy.com/github.com/vernesong/mihomo/releases/download/Prerelease-Alpha/mihomo-linux-amd64-alpha-smart-f83f0c7.gz" && gunzip mihomo.gz
 
-# 下载模型文件
+# 下载模型文件
 wget -O Model.bin "https://gh-proxy.com/github.com/vernesong/mihomo/releases/download/LightGBM-Model/Model.bin"
 
-# 移动并设置权限
+# 移动并设置权限
 mv -f mihomo /usr/bin/mihomo
 chmod 755 /usr/bin/mihomo
 
-# 重启服务
+# 重启服务
 /etc/init.d/nikki restart
 
-# 清理临时文件
+# 清理文件
 rm -f mihomo.gz
